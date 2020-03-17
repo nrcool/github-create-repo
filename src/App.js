5cd6204f8e63179e5f18d7f04c5d09e42d74f5ce
NAQVI <nomanalam7214@gmail.com>
2020-03-17 20:55:51 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'
+import "./app.css"
 
 export default class App extends Component {
 constructor(props) {
@@ -7,7 +8,7 @@ constructor(props) {
   super(props)
 
   this.state = {
-     username:"ciwwan1980 ",
+     username:"ciwwan1980",
      repo:[],
      url:"https://api.github.com/user/repos",
      repoUrl:"https://api.github.com/ciwwan1980/repos",    
@@ -32,13 +33,29 @@ componentDidMount(){
     console.log("render ....")
     return (
       <div>
-        <h1>Github API</h1>
+        <div className="header">
+            <h1>Github API</h1>
+            <button>Create git Repository</button>
+        </div>
+      
     <h2>UserName: {this.state.repo.length>0 ? this.state.repo[0].owner.login :"waiting"} </h2>
+    <img src={this.state.repo.length>0?this.state.repo[0].owner.avatar_url:"img"} alt="avatar" width="100" height="100"/>
+
+      <div className="repo">  <h1> Repository </h1>
+      <div className="rep"> 
+      <h3>Title: 1</h3>
+          <p>Description</p>
+          <p>Private</p>
+      </div>
+      <div className="rep"> 
+      <h3>Title: 2</h3>
+          <p>Description</p>
+          <p>Public</p>
+      </div>
+        </div>
+   
 
 
-
-
-    <img src={this.state.repo.length>0?this.state.repo[0].owner.avatar_url:"img"} alt="avatar" width="200" height="200"/>
       </div>
     )
   }
